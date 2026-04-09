a02cef9c5d03029b82ad17e72903bdd8012e5744
viniciusbduarte <vg39348@gmail.com>
2026-04-09 17:15:48 -0300

    atualizando materiais

---
+++ 03-periodo/teoria-grafos/material/meu_grafo_lista_adj_nao_dir.py
@@ -0,0 +1,65 @@
+from bibgrafo.grafo_lista_adj_nao_dir import GrafoListaAdjacenciaNaoDirecionado
+from bibgrafo.grafo_errors import *
+
+
+class MeuGrafo(GrafoListaAdjacenciaNaoDirecionado):
+
+    def vertices_nao_adjacentes(self):
+        '''
+        Provê um conjunto de vértices não adjacentes no grafo.
+        O conjunto terá o seguinte formato: {X-Z, X-W, ...}
+        Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+        :return: Um objeto do tipo set que contém os pares de vértices não adjacentes
+        '''
+        pass # Apague essa instrução e inicie seu código aqui
+
+    def ha_laco(self):
+        '''
+        Verifica se existe algum laço no grafo.
+        :return: Um valor booleano que indica se existe algum laço.
+        '''
+        for a in self.arestas.values():
+            if a.v1 == a.v2:
+                return True
+        return False
+
+    def grau(self, V=''):
+        '''
+        Provê o grau do vértice passado como parâmetro
+        :param V: O rótulo do vértice a ser analisado
+        :return: Um valor inteiro que indica o grau do vértice
+        :raises: VerticeInvalidoError se o vértice não existe no grafo
+        '''
+        if not self.existe_rotulo_vertice(V):
+            raise VerticeInvalidoError()
+
+        grau = 0
+        for a in self.arestas:
+            if self.arestas[a].v1.rotulo == V:
+                grau += 1
+            if self.arestas[a].v2.rotulo == V:
+                grau += 1
+        return grau
+
+    def ha_paralelas(self):
+        '''
+        Verifica se há arestas paralelas no grafo
+        :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+        '''
+        pass
+
+    def arestas_sobre_vertice(self, V):
+        '''
+        Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+        :param V: Um string com o rótulo do vértice a ser analisado
+        :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+        pass
+
+    def eh_completo(self):
+        '''
+        Verifica se o grafo é completo.
+        :return: Um valor booleano que indica se o grafo é completo
+        '''
+        pass
